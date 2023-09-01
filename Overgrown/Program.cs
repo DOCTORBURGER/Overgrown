9dfca256945da52ec18854cec2733c07b862e88d
Jonah Bogusch <jonahmbog@gmail.com>
2023-08-31 23:28:09 -0500

    Project init. Title animated and rendering.

---
+++ Overgrown/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Overgrown.OvergrownGame();
+game.Run();
---
+++ Overgrown/TitleSprite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Overgrown
+{
+    public class TitleSprite
+    {
+        private const float ANIMATION_SPEED = 0.3f;
+
+        private double animationTimer;
+
+        private int animationFrame;
+
+        private Vector2 position = new Vector2(0, 0);
+
+        private Texture2D texture;
+
+        public void LoadContent(ContentManager content)
+        {
+            texture = content.Load<Texture2D>("title");
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (animationTimer > ANIMATION_SPEED)
+            {
+                animationFrame++;
+                if (animationFrame >= 4) animationFrame = 0;
+                animationTimer -= ANIMATION_SPEED;
+            }
+
+            var source = new Rectangle(animationFrame * 800, 0, 800, 480);
+            spriteBatch.Draw(texture, position, source, Color.White);
+        }
+    }
+}
