55ff99bca8fba9efccb3dfadb9c80d95540d8f0d
Jonah Bogusch <jonahmbog@gmail.com>
2023-09-25 20:02:40 -0500

    Removed comments

--- Overgrown/OvergrownGame.cs
+++ Overgrown/OvergrownGame.cs
@@ -14,21 +14,6 @@ namespace Overgrown
         private GraphicsDeviceManager _graphics;
         private SceneManager sceneManager;
 
-        /*
-        private Title titleSprite;
-        private Sun sunSprite;
-        private Cloud cloudSprite;
-
-        private Button startButton;
-        private Button loadButton;
-        private Button optionsButton;
-        private Button exitButton;
-
-        private List<Button> buttons;
-
-        private Player playerSprite;
-        */
-
         public OvergrownGame()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -41,60 +26,17 @@ namespace Overgrown
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-            /*
-            titleSprite = new Title();
-            cloudSprite = new Cloud();
-            sunSprite= new Sun();
-            startButton = new Button("START", new Vector2(75, 75));
-            loadButton = new Button("LOAD", new Vector2(75 + 300 + 50, 75));
-            optionsButton = new Button("OPTIONS", new Vector2(75, 225));
-            exitButton = new Button("QUIT", new Vector2(75 + 300 + 50, 225));
-            exitButton.Click += ExitButton_Click;
-
-            buttons = new List<Button>();
-            buttons.Add(startButton);
-            buttons.Add(loadButton);
-            buttons.Add(optionsButton);
-            buttons.Add(exitButton);
-            
-            playerSprite = new Player();
-            */
-
             sceneManager.SetScene(new MainMenuScene());
 
             base.Initialize();
         }
 
-        protected override void LoadContent()
-        {
-            // TODO: use this.Content to load your game content here
-            /*
-            titleSprite.LoadContent(Content);
-            cloudSprite.LoadContent(Content);
-            sunSprite.LoadContent(Content);
-            startButton.LoadContent(Content);
-            loadButton.LoadContent(Content);
-            optionsButton.LoadContent(Content);
-            exitButton.LoadContent(Content);
-            playerSprite.LoadContent(Content)
-            */;
-        }
+        protected override void LoadContent() { }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-            /*
-            cloudSprite.Update(gameTime, GraphicsDevice);
-            foreach (var button in buttons)
-            {
-                button.Update(gameTime);
-            }
-            playerSprite.Update(gameTime, buttons);
-            */
 
             base.Update(gameTime);
         }
@@ -102,20 +44,6 @@ namespace Overgrown
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.DeepSkyBlue);
-
-            // TODO: Add your drawing code here
-            /*
-            _spriteBatch.Begin();
-            titleSprite.Draw(gameTime, _spriteBatch);
-            sunSprite.Draw(gameTime, _spriteBatch);
-            cloudSprite.Draw(gameTime, _spriteBatch);
-            startButton.Draw(_spriteBatch);
-            loadButton.Draw(_spriteBatch);
-            optionsButton.Draw(_spriteBatch);
-            exitButton.Draw(_spriteBatch);
-            playerSprite.Draw(gameTime, _spriteBatch);
-            _spriteBatch.End();
-            */
 
             base.Draw(gameTime);
         }
