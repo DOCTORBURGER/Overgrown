2037a2a17b02fcc4fe8b9e191a4f8091eda3625a
Jonah Bogusch <jonahmbog@gmail.com>
2023-10-22 18:56:46 -0500

    Added collisions code

---
+++ Overgrown/Collisions/BoundingRectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Overgrown.Collisions
+{
+    public struct BoundingRectangle
+    {
+        public float X, Y, Width, Height;
+
+        public float Left => X;
+
+        public float Right => X + Width;
+
+        public float Top => Y;
+
+        public float Bottom => Y + Height;
+
+        public BoundingRectangle(float x, float y, float width, float height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+        }
+
+        public BoundingRectangle(Vector2 position, float width, float height)
+        {
+            X = position.X;
+            Y = position.Y;
+            Width = width;
+            Height = height;
+        }
+
+        public bool CollidesWith(BoundingRectangle other)
+        {
+            return CollisionHelper.Collides(this, other);
+        }
+    }
+}
---
+++ Overgrown/Collisions/CollisionHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overgrown.Collisions
+{
+    public class CollisionHelper
+    {
+        public static bool Collides(BoundingRectangle a, BoundingRectangle b)
+        {
+            return !(a.Right < b.Left || a.Left > b.Right ||
+                a.Top > b.Bottom || a.Bottom < b.Top);
+        }
+    }
+}
