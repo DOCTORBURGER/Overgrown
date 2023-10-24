6c147bbf160b53d9c4146dc52d11fce47d03a2c3
Jonah Bogusch <jonahmbog@gmail.com>
2023-10-24 03:31:51 -0500

    Done with new settings. Fullscreen is janky, going to fix that later.

--- Overgrown/Managers/SceneManager.cs
+++ Overgrown/Managers/SceneManager.cs
@@ -2,53 +2,86 @@
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Overgrown.Scenes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Overgrown.State_Management;
 
 namespace Overgrown.Managers
 {
     public class SceneManager : DrawableGameComponent
     {
-        private IScene currentScene;
+        private IScene _currentScene;
 
-        private readonly ContentManager content;
+        private readonly ContentManager _content;
+        private GraphicsDeviceManager _graphics;
+        private Matrix _scaleMatrix = Matrix.Identity;
+
+        private readonly InputState _input = new InputState();
 
         public SpriteBatch SpriteBatch { get; private set; }
 
         public SpriteFont Font { get; private set; }
 
-        public SceneManager(Game game) : base(game)
+        public readonly Point VirtualResolution = new Point(800, 480);
+        public Matrix ScaleMatrix => _scaleMatrix;
+
+        public SceneManager(Game game, GraphicsDeviceManager graphics) : base(game)
         {
-            content = new ContentManager(game.Services, "Content");
+            _content = new ContentManager(game.Services, "Content");
+            _graphics = graphics;
         }
 
         protected override void LoadContent()
         {
             SpriteBatch = new SpriteBatch(GraphicsDevice);
-            Font = content.Load<SpriteFont>("button_font");     
+            Font = _content.Load<SpriteFont>("Silkscreen");
         }
 
         public void SetScene(IScene newScene)
         {
-            if (currentScene != null) { currentScene.UnloadContent(); }
+            if (_currentScene != null) { _currentScene.UnloadContent(); }
 
-            currentScene = newScene;
+            _currentScene = newScene;
             newScene.SceneManager = this;
 
-            currentScene.LoadContent();
+            _currentScene.LoadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
-            currentScene.Update(gameTime);
+            _input.Update();
+
+            _currentScene?.HandleInput(gameTime, _input);
+            _currentScene.Update(gameTime);
         }
 
         public override void Draw(GameTime gameTime)
         {
-            currentScene.Draw(gameTime);
+            _currentScene.Draw(gameTime);
+        }
+
+        public void CalculateMatrix()
+        {
+            float scaleX = (float)Game.GraphicsDevice.Viewport.Width / VirtualResolution.X;
+            float scaleY = (float)Game.GraphicsDevice.Viewport.Height / VirtualResolution.Y;
+
+            _scaleMatrix = Matrix.CreateScale(scaleX, scaleY, 1.0f);
+        }
+
+        public void SetResolution(Point resolution)
+        {
+            _graphics.PreferredBackBufferWidth = resolution.X;
+            _graphics.PreferredBackBufferHeight = resolution.Y;
+            _graphics.ApplyChanges();
+
+            CalculateMatrix();
+        }
+
+        public bool SetFullScreen()
+        {
+            _graphics.ToggleFullScreen();
+
+            _graphics.ApplyChanges();
+
+            return _graphics.IsFullScreen;
         }
     }
 }
--- Overgrown/Scenes/MainMenuScene.cs
+++ Overgrown/Scenes/MainMenuScene.cs
@@ -1,135 +1,37 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Media;
-using Overgrown.Entities;
-using Overgrown.Managers;
-using Overgrown.UI;
+﻿using Overgrown.UI;
 using System;
-using System.Collections.Generic;
-using Microsoft.Xna.Framework.Audio;
-using System.Linq;
-using System.Reflection.Metadata;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Overgrown.Scenes
 {
-    public class MainMenuScene : BaseScene
+    public class MainMenuScene : MenuScene
     {
-        private Title title;
-
-        private Button startButton;
-        private Button loadButton;
-        private Button optionsButton;
-        private Button exitButton;
-        private List<Button> buttons;
-
-        private Sun sunSprite;
-        private Cloud cloudSprite;
-
-        private Song backgroundMusic;
-
-        private ContentManager content;
-
-        public MainMenuScene()
+        public MainMenuScene() : base(new Title())
         {
-            title = new Title();
+            var startGameEntry = new MenuEntry("Start Game");
+            var settingsEntry = new MenuEntry("Settings");
+            var exitEntry = new MenuEntry("Exit");
 
-            startButton = new Button("START", new Vector2(75, 75));
-            startButton.Click += StartButton_Click;
+            startGameEntry.Selected += StartGame;
+            settingsEntry.Selected += Settings;
+            exitEntry.Selected += ExitClicked;
 
-            loadButton = new Button("LOAD", new Vector2(75 + 300 + 50, 75));
-
-            optionsButton = new Button("OPTIONS", new Vector2(75, 225));
-
-            exitButton = new Button("QUIT", new Vector2(75 + 300 + 50, 225));
-            exitButton.Click += ExitButton_Click;
-
-            buttons = new List<Button>
-            {
-                startButton,
-                loadButton,
-                optionsButton,
-                exitButton
-            };
-
-            cloudSprite = new Cloud();
-            sunSprite = new Sun();
+            _menuEntries.Add(startGameEntry);
+            _menuEntries.Add(settingsEntry);
+            _menuEntries.Add(exitEntry);
         }
-
-        public override void LoadContent()
-        {
-            if (content == null)
-                content = new ContentManager(SceneManager.Game.Services, "Content");
-
-            title.LoadContent(content);
-
-            startButton.LoadContent(content);
-            loadButton.LoadContent(content);
-            optionsButton.LoadContent(content);
-            exitButton.LoadContent(content);
-
-            cloudSprite.LoadContent(content);
-            sunSprite.LoadContent(content);
-
-            backgroundMusic = content.Load<Song>("Geb - Sequoias Start as Saplings");
-            MediaPlayer.Play(backgroundMusic);
-            MediaPlayer.IsRepeating = true;
-        }
-
-        public override void UnloadContent()
-        {
-            if (content == null)
-                content = new ContentManager(SceneManager.Game.Services, "Content");
-
-            exitButton.Click -= ExitButton_Click;
-
-            content.Unload();
-        }
-
-        public override void Update(GameTime gameTime) 
-        {
-            base.Update(gameTime);
-
-            cloudSprite.Update(gameTime, SceneManager.Game.GraphicsDevice);
-
-            foreach (var button in buttons)
-            {
-                button.Update(gameTime);
-            }
-
-            base.Update(gameTime);
-        }
-
-        public override void Draw(GameTime gameTime) 
-        {
-            SpriteBatch spriteBatch = SceneManager.SpriteBatch;
-
-            spriteBatch.Begin();
-
-            title.Draw(gameTime, spriteBatch);
-
-            sunSprite.Draw(gameTime, spriteBatch);
-
-            cloudSprite.Draw(gameTime, spriteBatch);
-
-            foreach (var button in buttons)
-            {
-                button.Draw(spriteBatch);
-            }
-
-            spriteBatch.End();
-        }
-
-        private void ExitButton_Click(object sender, System.EventArgs e)
-        {
-            SceneManager.Game.Exit();
-        }
-
-        private void StartButton_Click(object sender, System.EventArgs e)
+        private void StartGame(object sender, EventArgs e)
         {
             SceneManager.SetScene(new GameScene());
+        }
+
+        private void Settings(object sender, EventArgs e)
+        {
+            SceneManager.SetScene(new SettingsMenuScene());
+        }
+
+        private void ExitClicked(object sender, EventArgs e)
+        {
+            SceneManager.Game.Exit();
         }
     }
 }
